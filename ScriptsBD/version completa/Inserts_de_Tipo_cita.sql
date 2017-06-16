76c7e01dc7f277c8fe4321e4a0f48179c3778a2a
zahydo <santiagodorado@unicauca.edu.co>
2017-06-16 14:52:49 -0500

    :whale2: ¿Por que he hecho este cambio?: 1. Elimino filtro de hora de inicio y espera 2. Cambio de los inserts de tipo_cita_medica

---
+++ ScriptsBD/version completa/Inserts_de_Tipo_cita.sql
@@ -0,0 +1,7 @@
+DELETE FROM `cita_medica_med` WHERE id = 1
+
+DELETE FROM `tipo_cita_med`
+
+INSERT INTO `tipo_cita_med` (`ID`, `NOMBRE`) VALUES ('1', 'Primera vez');
+INSERT INTO `tipo_cita_med` (`ID`, `NOMBRE`) VALUES ('2', 'Control');
+INSERT INTO `tipo_cita_med` (`ID`, `NOMBRE`) VALUES ('3', 'Revisión Exam.');
